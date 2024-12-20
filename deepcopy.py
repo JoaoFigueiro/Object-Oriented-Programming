6a352b48ef9a14e17ad0d9b1dc2663feced516bc
JoaoFigueiro <joao.l.comiran@gmail.com>
2024-12-20 15:20:09 -0300

    feat: deepcopy

--- deepcopy.py
+++ deepcopy.py
@@ -51,3 +51,64 @@ for item in warehouse_b:
         item["price"] *= 0.8
     print(item)
 
+# 4.1.1.13 LAB #2
+import copy
+
+import copy
+
+
+class Delicacy:
+    def __init__(self, name, price, weight):
+        self.name = name
+        self.price = price
+        self.weight = weight
+
+    def __str__(self):
+        return 'item:{}, price:{}, total weight: {}'.format(
+            self.name, self.price, self.weight
+            )
+
+warehouse = list()
+
+warehouse.append(Delicacy('Lolly Pop', 0.4, 133))
+warehouse.append(Delicacy('Licorice', 0.1, 251))
+warehouse.append(Delicacy('Chocolate', 1, 601))
+warehouse.append(Delicacy('Sours', 0.01, 513))
+warehouse.append(Delicacy('Hard candies', 0.3, 433))
+
+new_warehouse = copy.deepcopy(warehouse)
+for item in new_warehouse:
+    if item.weight > 300:
+        item.price *= 0.8
+
+print('*' * 20)
+print('Source list of candies')
+for item in warehouse:
+    print(item)
+
+print('*' * 20)
+print('Price proposal')
+for item in new_warehouse:
+    print(item)
+
+# Check that the line:
+# new_warehouse = copy.deepcopy(warehouse)
+# is exchanged with the following line:
+# new_warehouse = copy.copy(warehouse)
+
+
+import shelve
+
+shelve_name = 'first_shelve.shlv'
+
+my_shelve = shelve.open(shelve_name, flag='c')
+my_shelve['EUR'] = {'code':'Euro', 'symbol': '€'}
+my_shelve['GBP'] = {'code':'Pounds sterling', 'symbol': '£'}
+my_shelve['USD'] = {'code':'US dollar', 'symbol': '$'}
+my_shelve['JPY'] = {'code':'Japanese yen', 'symbol': '¥'}
+my_shelve.close()
+
+new_shelve = shelve.open(shelve_name)
+print(new_shelve['USD'])
+new_shelve.close()
+
