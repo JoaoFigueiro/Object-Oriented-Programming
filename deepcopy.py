cd6a00c4bd6adf3ade12e09c7d4e742b7f41e1af
JoaoFigueiro <joao.l.comiran@gmail.com>
2024-12-20 11:29:23 -0300

    feat: lab 4.1.1.12 #1

---
+++ deepcopy.py
@@ -0,0 +1,53 @@
+import copy
+import time
+
+a_list = [(1,2,3) for x in range(1_000_000)]
+
+print('Single reference copy')
+time_start = time.time()
+b_list = a_list
+print('Execution time:', round(time.time() - time_start, 3))
+print('Memory chunks:', id(a_list), id(b_list))
+print('Same memory chunk?', a_list is b_list)
+
+print()
+
+print('Shallow copy')
+time_start = time.time()
+b_list = a_list[:]
+print('Execution time:', round(time.time() - time_start, 3))
+print('Memory chunks:', id(a_list), id(b_list))
+print('Same memory chunk?', a_list is b_list)
+
+print()
+
+print('Deep copy')
+time_start = time.time()
+b_list = copy.deepcopy(a_list)
+print('Execution time:', round(time.time() - time_start, 3))
+print('Memory chunks:', id(a_list), id(b_list))
+print('Same memory chunk?', a_list is b_list)
+
+
+# 4.1.1.12 LAB #1
+import copy
+
+warehouse = list()
+warehouse.append({'name': 'Lolly Pop', 'price': 0.4, 'weight': 133})
+warehouse.append({'name': 'Licorice', 'price': 0.1, 'weight': 251})
+warehouse.append({'name': 'Chocolate', 'price': 1, 'weight': 601})
+warehouse.append({'name': 'Sours', 'price': 0.01, 'weight': 513})
+warehouse.append({'name': 'Hard candies', 'price': 0.3, 'weight': 433})
+
+print('Source list of candies')
+for item in warehouse:
+    print(item)
+
+print("******************\nPrice proposal")
+
+warehouse_b = copy.deepcopy(warehouse)
+for item in warehouse_b:
+    if item["weight"] > 300:
+        item["price"] *= 0.8
+    print(item)
+
